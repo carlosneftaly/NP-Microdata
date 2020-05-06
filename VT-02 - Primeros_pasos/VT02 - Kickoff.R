b23893b9c9d2863e34914bb7c1907bd0124ed628
carlosneftaly <neftaly.lozano@gmail.com>
2020-05-06 23:02:42 +0200

    Update May 05

---
+++ VT-02 - Primeros_pasos/VT02 - Kickoff.R
@@ -0,0 +1,26 @@
+---
+  title: " . "
+subtitle: "  "
+author: '  '
+output:
+  xaringan::moon_reader:
+  #css: [ "css/my-theme.css", "css/rutgers.css"]
+  # css: ["css/rutgers.css", #"css/custom.css"]
+  css: ["default","css/rutgers.css", "css/rutgers-fonts.css"]
+lib_dir: libs
+# yolo: TRUE
+nature:
+  highlightStyle: github
+highlightLines: true
+countIncrementalSlides: false
+ratio: "16:9"
+beforeInit: "https://platform.twitter.com/widgets.js"
+includes:
+  in_header: header.html  
+
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+```
+class: middle center 
